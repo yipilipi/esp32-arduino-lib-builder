f150846d9472f47c3ec3abd4c38f18983dc655b5
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2020-11-21 18:52:17 +0100

    rm docs

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -25,11 +25,11 @@ if [ -d "out" ]; then
     echo "$AR_BRANCH_SUFFIX"
     git clone https://github.com/tasmota/arduino-esp32 -b $AR_BRANCH_SUFFIX
     rm -rf arduino-esp32/tools/sdk
+    rm -rf arduino-esp32/docs
     cp -Rf tools/sdk arduino-esp32/tools/sdk
     cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
     mv arduino-esp32/ framework-arduinoespressif32/
     tar --exclude=.* -zcf ../$pio_archive_path framework-arduinoespressif32/
-    #zip -r $pio_archive_path framework-arduinoespressif32
     cd ..
 fi
 
