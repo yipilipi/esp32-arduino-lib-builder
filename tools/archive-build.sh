e3e4ddc5dcff21fbcde2e35a9d604334f16fc649
Christian Baars <Baars@gmx.de>
2020-11-21 17:58:43 +0100

    Update archive-build.sh

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -6,7 +6,7 @@ IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD)
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 archive_path="dist/arduino-esp32-libs-$idf_version_string.tar.gz"
 build_archive_path="dist/arduino-esp32-build-$idf_version_string.tar.gz"
-pio_archive_path="dist/framework-arduinoespressif32-$IDF_BRANCH.zip"
+pio_archive_path="dist/framework-arduinoespressif32-$idf_version_string.tar.gz"
 
 mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 if [ -d "out" ]; then
