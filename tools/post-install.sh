ed7d231fb75656a0aa809e7e14c89c6c87e02cf2
Christian Baars <Baars@gmx.de>
2020-11-21 11:16:49 +0100

    Create post-install.sh

---
+++ tools/post-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+source ./tools/config.sh
+
+if [ -d "out" ]; then
+    cd out
+    echo "POST INSTALL"
+    AR_BRANCH_SUFFIX = "idf-$IDF_BRANCH"
+    git clone "https://github.com/tasmota/arduino-esp32 -b $AR_BRANCH_SUFFIX"
+    rm -rf arduino-esp32/tools/sdk
+    cp -Rf tools/sdk arduino-esp32/tools/sdk
+    tar --exclude=".*" -zcvf arduino-esp32.tar.gz arduino-esp32/
+    cd ..
+fi
