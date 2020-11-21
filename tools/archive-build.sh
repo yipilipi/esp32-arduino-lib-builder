f69c8c62d2ea31b5691f4965d78d1aca7917d0b4
Christian Baars <Baars@gmx.de>
2020-11-21 16:36:51 +0100

    Update archive-build.sh

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -6,7 +6,7 @@ IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD)
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 archive_path="dist/arduino-esp32-libs-$idf_version_string.tar.gz"
 build_archive_path="dist/arduino-esp32-build-$idf_version_string.tar.gz"
-pio_archive_path="dist/arduino-ESP32-$idf_version_string.tar.gz"
+pio_archive_path="dist/framework-arduinoespressif32-$idf_version_string.tar.gz"
 
 mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 if [ -d "out" ]; then
@@ -18,12 +18,17 @@ fi
 
 if [ -d "out" ]; then
     cd out
-    echo "POST INSTALL"
+    echo "Creating framework-arduinoespressif32"
     AR_BRANCH_SUFFIX="idf-$IDF_BRANCH"
+    echo "$AR_BRANCH_SUFFIX"
     AR_BRANCH_SUFFIX=${AR_BRANCH_SUFFIX%"$-solo1"}
-    git clone "https://github.com/tasmota/arduino-esp32 -b $AR_BRANCH_SUFFIX"
+    echo "$AR_BRANCH_SUFFIX"
+    git clone https://github.com/tasmota/arduino-esp32 -b $AR_BRANCH_SUFFIX
     rm -rf arduino-esp32/tools/sdk
     cp -Rf tools/sdk arduino-esp32/tools/sdk
-    tar --exclude=".*" -zcvf pio_archive_path arduino-esp32/
+    cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
+    mv arduino-esp32/ framework-arduinoespressif32/
+    tar --exclude=.* -zcf ../$pio_archive_path framework-arduinoespressif32/
     cd ..
 fi
+
