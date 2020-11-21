670c33fe2daf02ca61984cee88dcafb75bd30785
esp32-arduino-lib-builder <runner@fv-az16-763.wc2tpadc5vle3op2acko2likyf.bx.internal.cloudapp.net>
2020-11-21 17:21:03 +0000

    esp32-arduino-lib idf 3.3-solo1

--- tools/current_commit.sh
+++
@@ -1,2 +0,0 @@
-IDF_COMMIT=$(wget -q -O- "https://github.com/jason2866/arduino-esp32/search?q=update+idf&type=Commits" | grep -i "update idf" | grep -e "to [0-9a-f]*" | sed "s/^.*to \([0-9a-f]*\).*/\1/" | head -1)
-echo Current commit is $IDF_COMMIT
