3f39f63c71592f0582d6889f9da68a7586d36716
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-01-18 15:38:45 +0100

    next naming try

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -4,15 +4,12 @@ IDF_COMMIT=$(git -C "$IDF_PATH" rev-parse --short HEAD)
 IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD)
 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
-echo "$idf_version_string"
-idf_solo1_string=${idf_version_string%"-solo1"}
-echo "$idf_solo1_string"
-archive_path="dist/arduino-esp32-libs-$idf_solo1_string.tar.gz"
-build_archive_path="dist/arduino-esp32-build-$idf_solo1_string.tar.gz"
-pio_archive_path="dist/framework-arduinoespressif32-$idf_solo1_string.tar.gz"
-echo "$archive_path%"
-echo "$build_archive_path%"
-echo "$pio_archive_path%"
+archive_path="dist/arduino-esp32-libs-$idf_version_string-solo1.tar.gz"
+build_archive_path="dist/arduino-esp32-build-$idf_version_string-solo1.tar.gz"
+pio_archive_path="dist/framework-arduinoespressif32-$idf_version_string-solo1.tar.gz"
+echo "$archive_path"
+echo "$build_archive_path"
+echo "$pio_archive_path"
 
 mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 #if [ -d "out" ]; then
