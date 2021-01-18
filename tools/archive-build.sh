024fb9734af6ad9f220369fece8c3bc8f5784a6c
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-01-18 15:25:02 +0100

    next try to get solo1 in name

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -5,11 +5,14 @@ IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD)
 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
 echo "$idf_version_string"
-idf_version_string=${idf_version_string%"-solo1"}
-echo "$idf_version_string"
-archive_path="dist/arduino-esp32-libs-$idf_version_string.tar.gz"
-build_archive_path="dist/arduino-esp32-build-$idf_version_string.tar.gz"
-pio_archive_path="dist/framework-arduinoespressif32-$idf_version_string.tar.gz"
+idf_solo1_string=${idf_version_string%"-solo1"}
+echo "$idf_solo1_string"
+archive_path="dist/arduino-esp32-libs-$idf_solo1_string.tar.gz"
+build_archive_path="dist/arduino-esp32-build-$idf_solo1_string.tar.gz"
+pio_archive_path="dist/framework-arduinoespressif32-$idf_solo1_string.tar.gz"
+echo "$archive_path%"
+echo "$build_archive_path%"
+echo "$pio_archive_path%"
 
 mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 #if [ -d "out" ]; then
