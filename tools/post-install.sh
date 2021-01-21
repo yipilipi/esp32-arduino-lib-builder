33f266e55ed769b8746393d08b0f7355b4939135
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-01-21 17:54:41 +0100

    Use branch master

--- tools/post-install.sh
+++ tools/post-install.sh
@@ -6,7 +6,8 @@ if [ -d "out" ]; then
     echo "POST INSTALL"
     AR_BRANCH_SUFFIX = "idf-$IDF_BRANCH"
     #AR_BRANCH_SUFFIX=${AR_BRANCH_SUFFIX%"$-solo1"}
-    git clone "https://github.com/espressif/arduino-esp32 -b $AR_BRANCH_SUFFIX"
+    #git clone "https://github.com/espressif/arduino-esp32 -b $AR_BRANCH_SUFFIX"
+    git clone "https://github.com/espressif/arduino-esp32"
     rm -rf arduino-esp32/tools/sdk
     cp -Rf tools/sdk arduino-esp32/tools/sdk
     tar --exclude=".*" -zcvf arduino-esp32.tar.gz arduino-esp32/
