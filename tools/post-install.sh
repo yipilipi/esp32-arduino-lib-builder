4f4b06c3b402d1df584fa09735e13617cebc88ee
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-01-21 18:05:02 +0100

    Update post-install.sh

--- tools/post-install.sh
+++ tools/post-install.sh
@@ -7,7 +7,7 @@ if [ -d "out" ]; then
     AR_BRANCH_SUFFIX = "idf-$IDF_BRANCH"
     #AR_BRANCH_SUFFIX=${AR_BRANCH_SUFFIX%"$-solo1"}
     #git clone "https://github.com/espressif/arduino-esp32 -b $AR_BRANCH_SUFFIX"
-    git clone "https://github.com/espressif/arduino-esp32"
+    git clone "https://github.com/espressif/arduino-esp32 -b master"
     rm -rf arduino-esp32/tools/sdk
     cp -Rf tools/sdk arduino-esp32/tools/sdk
     tar --exclude=".*" -zcvf arduino-esp32.tar.gz arduino-esp32/
