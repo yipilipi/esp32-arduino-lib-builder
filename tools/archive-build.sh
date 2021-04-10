480d1580a09d3f70e23fc9b835f1e0dbf53ccef4
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-04-10 14:03:48 +0200

    Path update

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -4,9 +4,9 @@ IDF_COMMIT=$(git -C "$IDF_PATH" rev-parse --short HEAD)
 IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD)
 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
-archive_path="dist/arduino-esp32-libs-solo1-$idf_version_string.tar.gz"
-build_archive_path="dist/arduino-esp32-build-solo1-$idf_version_string.tar.gz"
-pio_archive_path="dist/framework-arduinoespressif32-solo1-$idf_version_string.tar.gz"
+archive_path="dist/arduino-esp32-libs-$idf_version_string.tar.gz"
+build_archive_path="dist/arduino-esp32-build-$idf_version_string.tar.gz"
+pio_archive_path="dist/framework-arduinoespressif32-$idf_version_string.tar.gz"
 echo "$archive_path"
 echo "$build_archive_path"
 echo "$pio_archive_path"
@@ -22,16 +22,7 @@ mkdir -p dist && rm -rf "$archive_path" "$build_archive_path"
 if [ -d "out" ]; then
     cd out
     echo "Creating framework-arduinoespressif32"
-    AR_BRANCH_SUFFIX="idf-$IDF_BRANCH"
-    echo "$AR_BRANCH_SUFFIX"
-    #AR_BRANCH_SUFFIX=${AR_BRANCH_SUFFIX%"-solo1"}
-    #echo "$AR_BRANCH_SUFFIX"
-    #git clone https://github.com/espressif/arduino-esp32 -b $AR_BRANCH_SUFFIX
     git clone https://github.com/espressif/arduino-esp32 -b idf-release/v3.3
-    # use specific commit (=release 1.0.5). Changes after did broke stuff. https://github.com/espressif/arduino-esp32/issues/4920
-    #cd arduino-esp32
-    #git reset --hard 419ba324
-    #cd ..
     rm -rf arduino-esp32/tools/sdk
     rm -rf arduino-esp32/docs
     cp -Rf tools/sdk arduino-esp32/tools/sdk
