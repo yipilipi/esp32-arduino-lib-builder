8e75ce6ff89ae413ec303e65820e3173f97c7e39
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-04-23 21:59:39 +0200

    Update post-install.sh

--- tools/post-install.sh
+++ tools/post-install.sh
@@ -5,8 +5,7 @@ if [ -d "out" ]; then
     cd out
     echo "POST INSTALL"
     AR_BRANCH_SUFFIX = "idf-$IDF_BRANCH"
-    AR_BRANCH_SUFFIX=${AR_BRANCH_SUFFIX%"$-solo1"}
-    git clone "https://github.com/tasmota/arduino-esp32 -b $AR_BRANCH_SUFFIX"
+    git clone "https://github.com/espressif/arduino-esp32 -b master"
     rm -rf arduino-esp32/tools/sdk
     cp -Rf tools/sdk arduino-esp32/tools/sdk
     tar --exclude=".*" -zcvf arduino-esp32.tar.gz arduino-esp32/
