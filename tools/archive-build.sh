d7bfe3492ff770cd79b3592e88bb584da503ec8f
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-06-12 14:10:54 +0200

    Tasmota framework
    added LITTLEFS

--- tools/archive-build.sh
+++ tools/archive-build.sh
@@ -4,9 +4,9 @@ IDF_COMMIT=$(git -C "$IDF_PATH" rev-parse --short HEAD)
 IDF_BRANCH=$(git -C "$IDF_PATH" symbolic-ref --short HEAD)
 
 idf_version_string=${IDF_BRANCH//\//_}"-$IDF_COMMIT"
-archive_path="dist/arduino-esp32-libs-solo1-$idf_version_string.tar.gz"
-build_archive_path="dist/arduino-esp32-build-solo1-$idf_version_string.tar.gz"
-pio_archive_path="dist/framework-arduinoespressif32-solo1-$idf_version_string.tar.gz"
+archive_path="dist/tasmota-esp32-libs-solo1-$idf_version_string.tar.gz"
+build_archive_path="dist/tasmota-esp32-build-solo1-$idf_version_string.tar.gz"
+pio_archive_path="dist/tasmota-arduinoespressif32-solo1-$idf_version_string.tar.gz"
 echo "$archive_path"
 echo "$build_archive_path"
 echo "$pio_archive_path"
@@ -35,6 +35,11 @@ if [ -d "out" ]; then
     cp -Rf tools/platformio-build.py arduino-esp32/tools/platformio-build.py
     cp -Rf platform.txt arduino-esp32/platform.txt
     cp ../core_version.h arduino-esp32/cores/esp32/core_version.h
+    echo "add LITTLEFS as library to framework"
+    git clone https://github.com/Jason2866/LITTLEFS
+    ls LITTLEFS/*
+    mkdir -p arduino-esp32/libraries/LITTLEFS
+    cp -Rf LITTLEFS/* arduino-esp32/libraries/LITTLEFS
     mv arduino-esp32/ framework-arduinoespressif32/
     tar --exclude=.* -zcf ../$pio_archive_path framework-arduinoespressif32/
     cd ..
