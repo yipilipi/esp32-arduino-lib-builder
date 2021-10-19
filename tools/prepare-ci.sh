e80e01e20dbc668d3d48f35cbcd5c0be3a3b684a
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-10-19 22:47:59 +0200

    cryptography==3.4.8

--- tools/prepare-ci.sh
+++ tools/prepare-ci.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install -y git wget curl libssl-dev libncurses-dev flex bison gperf python3 cmake ninja-build ccache
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py && \
-pip3 install setuptools pyserial click future wheel cryptography pyparsing==2.3.1 pyelftools
+pip3 install setuptools pyserial click future wheel cryptography==3.4.8 pyparsing==2.3.1 pyelftools
