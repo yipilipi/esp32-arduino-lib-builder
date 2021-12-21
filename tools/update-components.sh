1cac1f522854f5c39afe65c6c2adce06adede503
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2021-12-21 19:27:09 +0100

    rm rainmaker reminents

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -3,8 +3,6 @@
 source ./tools/config.sh
 
 CAMERA_REPO_URL="https://github.com/espressif/esp32-camera.git"
-#FACE_REPO_URL="https://github.com/espressif/esp-dl.git"
-#RMAKER_REPO_URL="https://github.com/espressif/esp-rainmaker.git"
 DSP_REPO_URL="https://github.com/espressif/esp-dsp.git"
 LITTLEFS_REPO_URL="https://github.com/joltwallet/esp_littlefs.git"
 TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
@@ -63,16 +61,6 @@ else
     git -C "$AR_COMPS/esp_littlefs" submodule update --init --recursive
 fi
 if [ $? -ne 0 ]; then exit 1; fi
-
-#
-# CLONE/UPDATE ESP-RAINMAKER
-#
-
-#if [ ! -d "$AR_COMPS/esp-rainmaker" ]; then
-#    git clone $RMAKER_REPO_URL "$AR_COMPS/esp-rainmaker"
-#    git -C "$AR_COMPS/esp-rainmaker" checkout f1b82c71c4536ab816d17df016d8afe106bd60e3
-#fi
-#if [ $? -ne 0 ]; then exit 1; fi
 
 #
 # CLONE/UPDATE ESP-DSP
