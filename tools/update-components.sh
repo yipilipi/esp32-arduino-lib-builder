09c322456d893342d14bc20737845ddb11f9827b
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-01-16 12:00:10 +0100

    use tasmota espcam fork
    which uses performance as compile option

--- tools/update-components.sh
+++ tools/update-components.sh
@@ -2,7 +2,7 @@
 
 source ./tools/config.sh
 
-CAMERA_REPO_URL="https://github.com/espressif/esp32-camera.git"
+CAMERA_REPO_URL="https://github.com/tasmota/esp32-camera.git"
 DSP_REPO_URL="https://github.com/espressif/esp-dsp.git"
 LITTLEFS_REPO_URL="https://github.com/joltwallet/esp_littlefs.git"
 TINYUSB_REPO_URL="https://github.com/hathach/tinyusb.git"
